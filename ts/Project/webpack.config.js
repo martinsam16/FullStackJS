60154d211e87c492f77c86f2717512119de87774
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2019-08-25 19:12:59 -0500

    proyecto en ts culminado

--- ts/Project/webpack.config.js
+++ ts/Project/webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
         path:'./dist',
         filename:'app.bundle.js',
     },
+    resolve:{
+        extensions:['', '.webpack.js', '.web.js', '.ts', '.js']
+    },
     module: {
         loaders: [
             {
